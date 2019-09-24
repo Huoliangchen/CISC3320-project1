39791bcff6f3ebe2ba9b2c2f9a2a7668686987d3
Huoliangchen <35934291+Huoliangchen@users.noreply.github.com>
2019-09-24 13:58:49 -0400

    Update shellmian.c

--- shellmian.c
+++ shellmian.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+
 #define MAX 50
 
-char **commandSplit(char *);
+char **commandSplit(char []);
+void execute(char **);
 
 int main()
 {
@@ -11,27 +13,27 @@ int main()
 	char **command;
 	int state = 1;
 	
-	char *commandBuiltins[] = {"list", "history", "printwd", "chdir"};
+	char *commandBuiltins[] = {"list", "history", "printwd", "chdir", "complie", "run", "\0"};
 	
 	while(state)
-	{
-		printf("Myshell> ")
+	{	
+		printf("Myshell> ");
 		fgets(input, MAX, stdin);
+    if(input[strlen(input) - 1] == '\n')
+          input[strlen(input) - 1] = '\0';
+
 		command = commandSplit(input);
-		
-		int i = 0;
-		
-		while(command[i])
-		{
-			printf("%d is %s ", i,command[i]);
-			i++;
-		}
+    if (!command[0]) {
+            continue;
+        }
+        
+		execute(command);
 	}
 	
 	return 0;
 }
 
-char **commandSplit(char * input)
+char **commandSplit(char *input)
 {
 	int index = 0;
 	char **command = malloc(MAX * sizeof(input));
@@ -50,4 +52,28 @@ char **commandSplit(char * input)
 	
 	
 	return command;
+}
+
+void execute(char **command)
+{ 
+  if(strcmp(command[0], "chdir") == 0){
+    printf("cd");
+  }
+	else if(strcmp(command[0], "list") == 0){
+    printf("ls");
+  }
+  else if(strcmp(command[0], "history") == 0){
+    printf("his");
+  }
+  else if(strcmp(command[0], "printwd") == 0){
+    printf("wd");
+  }
+  else if(strcmp(command[0], "complie") == 0){
+    printf("complie");
+  }
+  else if(strcmp(command[0], "run") == 0){
+    printf("run");
+  }else{
+    printf("command doese not exist.\n");
+  }
 }
