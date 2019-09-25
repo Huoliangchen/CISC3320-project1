3ee682c36465a6b8e552ab126bb8b9a11a6b562e
Huoliangchen <35934291+Huoliangchen@users.noreply.github.com>
2019-09-25 10:11:51 -0400

    Update shellmian.c

--- shellmian.c
+++ shellmian.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 #define MAX 50
+
 
 char **commandSplit(char []);
 void execute(char **);
